46a257bb50afdc58269b15f17a537c9b8c2f6bd2
samu-rize <6710@holbertonstudents.com>
2024-03-11 17:50:38 +0100

    change logo

--- client/src/Header.js
+++ client/src/Header.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Header(){
     return (
     <header>
-        <Link to="/" className="logo">Nika Blog</Link>
+        <Link to="/" className="logo">ANI VIBES</Link>
         <nav>
             <Link to="/login">Login</Link>
             <Link to="/register">Register</Link>
